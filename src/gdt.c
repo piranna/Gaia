7a620708633d51071366145d632d512ae6b9b5d9
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-12-19 00:37:55 +0100

    Enable switch to userspace (althought it doesn't work...)

--- src/gdt.c
+++ src/gdt.c
@@ -7,31 +7,38 @@
 
 #include "common.h"
 #include "gdt.h"
+#include "tss.h"
 
 
 // Lets us access our ASM functions from our C code.
 extern void gdt_flush(u32int);
+extern void tss_flush();
 
 // Internal function prototypes.
 static void gdt_set_gate(s32int,u32int,u32int,u8int,u8int);
+static void tss_write(s32int,u16int,u32int);
 
 
 gdt_entry_t gdt_entries[5];
 gdt_ptr_t   gdt_ptr;
+tss_entry_t tss_entry;
 
 
 void gdt_init(void)
 {
-    gdt_ptr.limit = (sizeof(gdt_entry_t) * 5) - 1;
+    gdt_ptr.limit = (sizeof(gdt_entry_t) * 6) - 1;
     gdt_ptr.base  = (u32int)&gdt_entries;
 
-    gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
+    gdt_set_gate(0, 0, 0,          0,    0   ); // Null segment
     gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
     gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
     gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
     gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
 
+    tss_write(5, 0x10, 0x0);
+
     gdt_flush((u32int)&gdt_ptr);
+    tss_flush();
 }
 
 // Set the value of one GDT entry.
@@ -45,5 +52,38 @@ static void gdt_set_gate(s32int num, u32int base, u32int limit, u8int access, u8
     gdt_entries[num].granularity = (limit >> 16) & 0x0F;
     
     gdt_entries[num].granularity |= gran & 0xF0;
-    gdt_entries[num].access      = access;
+    gdt_entries[num].access       = access;
 }
+
+
+// Initialise our task state segment structure.
+static void tss_write(s32int num, u16int ss0, u32int esp0)
+{
+	// Firstly, let's compute the base and limit of our entry into the GDT.
+	u32int base = (u32int)&tss_entry;
+	u32int limit = base + sizeof(tss_entry);
+
+	// Now, add our TSS descriptor's address to the GDT.
+	gdt_set_gate(num, base, limit, 0xE9, 0x00);
+
+	// Ensure the descriptor is initially zero.
+	memset((u8int*)&tss_entry, 0, sizeof(tss_entry));
+
+	tss_entry.ss0  = ss0;  // Set the kernel stack segment.
+	tss_entry.esp0 = esp0; // Set the kernel stack pointer.
+
+	// Here we set the cs, ss, ds, es, fs and gs entries in the TSS. These
+	// specify what segments should be loaded when the processor switches to
+	// kernel mode. Therefore they are just our normal kernel code/data segments
+	// - 0x08 and 0x10 respectively, but with the last two bits set, making 0x0b
+	// and 0x13. The setting of these bits sets the RPL (requested privilege
+	// level) to 3, meaning that this TSS can be used to switch to kernel mode
+	// from ring 3.
+	tss_entry.cs = 0x0b;
+	tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs = 0x13;
+}
+
+//void set_kernel_stack(u32int stack)
+//{
+//   tss_entry.esp0 = stack;
+//}
---
+++ src/include/tss.h
@@ -0,0 +1,45 @@
+/*
+ * tss.h
+ *
+ *  Created on: 06/12/2011
+ *      Author: piranna
+ */
+
+#ifndef TSS_H_
+#define TSS_H_
+
+// A struct describing a Task State Segment.
+struct tss_entry_struct
+{
+   u32int prev_tss;   // The previous TSS - if we used hardware task switching this would form a linked list.
+   u32int esp0;       // The stack pointer to load when we change to kernel mode.
+   u32int ss0;        // The stack segment to load when we change to kernel mode.
+   u32int esp1;       // Unused...
+   u32int ss1;
+   u32int esp2;
+   u32int ss2;
+   u32int cr3;
+   u32int eip;
+   u32int eflags;
+   u32int eax;
+   u32int ecx;
+   u32int edx;
+   u32int ebx;
+   u32int esp;
+   u32int ebp;
+   u32int esi;
+   u32int edi;
+   u32int es;         // The value to load into ES when we change to kernel mode.
+   u32int cs;         // The value to load into CS when we change to kernel mode.
+   u32int ss;         // The value to load into SS when we change to kernel mode.
+   u32int ds;         // The value to load into DS when we change to kernel mode.
+   u32int fs;         // The value to load into FS when we change to kernel mode.
+   u32int gs;         // The value to load into GS when we change to kernel mode.
+   u32int ldt;        // Unused...
+   u16int trap;
+   u16int iomap_base;
+} __attribute__((packed));
+
+typedef struct tss_entry_struct tss_entry_t;
+
+#endif /* TSS_H_ */
