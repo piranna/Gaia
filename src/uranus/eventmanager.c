a9b53f1fd8409c7e3b278587f42e7e55adfd5d39
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2011-12-25 17:19:53 +0100

    Mock-up of keyboard working by events

--- src/uranus/eventmanager.c
+++ src/uranus/eventmanager.c
@@ -10,15 +10,30 @@
 #include <string.h>
 
 #include "fixedDict.h"
+#include "syscall.h"
 
+/* hardcoded */
+#define IRQ1  33	// keyboard
+/* hardcoded */
 
 static pairKeyValue eventmanager_events_pairs[10];
 static fixedDict eventmanager_events;
+
+#include "drivers/keyboard.h"
+static void eventmanager_irq_handler(registers_t* regs)
+{
+////	char event[7];
+////	snprintf("IRQ/%d", regs->int_no);
+////	eventmanager_send(event, regs->err_code);
+	eventmanager_send("IRQ/1", regs->err_code);
+}
 
 
 void eventmanager_init(void)
 {
 	fixedDict_init(&eventmanager_events, eventmanager_events_pairs);
+
+	syscall_irq_register_handler(IRQ1, &eventmanager_irq_handler);
 }
 
 
