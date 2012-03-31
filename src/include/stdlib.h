27ba8637986940dd8991d82d65bb09b44706cfa5
piranna <piranna@bc3f1f7f-79ad-4f56-a4c9-7031929290b6>
2012-03-31 16:41:48 +0000

    Moved itoa() to stdlib.h
    git-svn-id: https://forja.rediris.es/svn/cusl6-gaia/trunk@27 bc3f1f7f-79ad-4f56-a4c9-7031929290b6

---
+++ src/include/stdlib.h
@@ -0,0 +1,16 @@
+/*
+ * stdlib.h
+ *
+ *  Created on: 19/12/2011
+ *      Author: piranna
+ */
+
+#ifndef STDLIB_H_
+#define STDLIB_H_
+
+/* Convert the integer D to a string and save the string in BUF. If
+	BASE is equal to 'd', interpret that D is decimal, and if BASE is
+	equal to 'x', interpret that D is hexadecimal. */
+void itoa(char* buf, int base, int d);
+
+#endif /* STDLIB_H_ */
