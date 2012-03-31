4f60274c5bcf1ecf8163e3cfae9b1451cb2d6001
piranna <piranna@bc3f1f7f-79ad-4f56-a4c9-7031929290b6>
2012-03-31 16:47:31 +0000

    Added TestAndSet
    git-svn-id: https://forja.rediris.es/svn/cusl6-gaia/trunk@90 bc3f1f7f-79ad-4f56-a4c9-7031929290b6

---
+++ src/asm/include/TestAndSet.h
@@ -0,0 +1,13 @@
+/*
+ * TestAndSet.h
+ *
+ *  Created on: 07/02/2012
+ *      Author: piranna
+ */
+
+#ifndef TESTANDSET_H_
+#define TESTANDSET_H_
+
+int TestAndSet(int* lock);
+
+#endif /* TESTANDSET_H_ */
