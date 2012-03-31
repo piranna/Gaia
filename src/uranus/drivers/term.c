1d0abcab7822278afaf91b8c1f8617edaa55ac4f
piranna <piranna@bc3f1f7f-79ad-4f56-a4c9-7031929290b6>
2012-03-31 23:15:21 +0000

    Commented unused event
    git-svn-id: https://forja.rediris.es/svn/cusl6-gaia/trunk@108 bc3f1f7f-79ad-4f56-a4c9-7031929290b6

--- src/uranus/drivers/term.c
+++ src/uranus/drivers/term.c
@@ -71,9 +71,9 @@ static void term_handler_press(unsigned char scancode)
 	}
 
 	char c = kbd[scancode];
-//	printf("term_handler_press: %u %c\n", scancode, c);
+//	printf("term_handler_press: '%u' '%c'\n", scancode, c);
 
-	eventmanager_send("term/press/character", c);
+//	eventmanager_send("term/press/character", c);
 
     eventmanager_send("VGA/text/putchar", c);
 }
