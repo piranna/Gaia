20618c6ebc4fce405c6793d79564d6dd236c10a1
piranna <piranna@bc3f1f7f-79ad-4f56-a4c9-7031929290b6>
2012-03-31 16:48:28 +0000

    Moved keyboard code to term (i don't know why init is not launched...)
    git-svn-id: https://forja.rediris.es/svn/cusl6-gaia/trunk@100 bc3f1f7f-79ad-4f56-a4c9-7031929290b6

--- src/uranus/drivers/keyboard.c
+++ src/uranus/drivers/keyboard.c
@@ -5,35 +5,37 @@
  * Notes: No warranty expressed or implied. Use at own risk.
  */
 
-#include "syscall.h"
-
-
-/* Handles the keyboard interrupt */
-static void keyboard_handler(void)
-{
-    /* Read from the keyboard's data buffer */
-    unsigned char scancode = syscall_inb(0x60);
-
-    /* If the top bit of the byte we read from the keyboard is
-    *  set, that means that a key has just been released */
-    if(scancode & 0x80)
-    {
-        scancode -= 0x80;   // Get key from scancode
-
-    	eventmanager_send("keyboard/release/scancode",scancode);
-    }
-    else
-    {
-        /* Here, a key was just pressed. Please note that if you
-        *  hold a key down, you will get repeated key press
-        *  interrupts. */
-
-    	eventmanager_send("keyboard/press/scancode",scancode);
-    }
-}
-
-/* Installs the keyboard handler into IRQ1 */
-void keyboard_init(void)
-{
-	eventmanager_attach("IRQ/1", &keyboard_handler);
-}
+//#include "syscall.h"
+//
+//
+///* Handles the keyboard interrupt */
+//static void keyboard_handler(int dumb)
+//{
+//	printf("keyboard_handler\n");
+//    /* Read from the keyboard's data buffer */
+//    unsigned char scancode = syscall_inb(0x60);
+//
+//    /* If the top bit of the byte we read from the keyboard is
+//    *  set, that means that a key has just been released */
+//    if(scancode & 0x80)
+//    {
+//        scancode -= 0x80;   // Get key from scancode
+//
+//    	eventmanager_send("keyboard/release/scancode",scancode);
+//    }
+//    else
+//    {
+//        /* Here, a key was just pressed. Please note that if you
+//        *  hold a key down, you will get repeated key press
+//        *  interrupts. */
+//
+//    	eventmanager_send("keyboard/press/scancode",scancode);
+//    }
+//}
+//
+///* Installs the keyboard handler into IRQ1 */
+//void keyboard_init(void)
+//{
+//	printf("keyboard_init\n");
+//	eventmanager_attach("irq/1", keyboard_handler);
+//}
--- src/uranus/drivers/term.c
+++ src/uranus/drivers/term.c
@@ -10,6 +10,35 @@
 #include "keyboard_layouts.h"
 
 
+#include "syscall.h"
+
+
+/* Handles the keyboard interrupt */
+static void keyboard_handler(int dumb)
+{
+	printf("keyboard_handler\n");
+    /* Read from the keyboard's data buffer */
+    unsigned char scancode = syscall_inb(0x60);
+
+    /* If the top bit of the byte we read from the keyboard is
+    *  set, that means that a key has just been released */
+    if(scancode & 0x80)
+    {
+        scancode -= 0x80;   // Get key from scancode
+
+    	eventmanager_send("keyboard/release/scancode",scancode);
+    }
+    else
+    {
+        /* Here, a key was just pressed. Please note that if you
+        *  hold a key down, you will get repeated key press
+        *  interrupts. */
+
+    	eventmanager_send("keyboard/press/scancode",scancode);
+    }
+}
+
+
 extern keyboard_layout kbdus;
 
 // Use normal keyboard by default
@@ -74,6 +103,8 @@ static void term_handler_release(unsigned char scancode)
 
 void term_init(void)
 {
+	eventmanager_attach("irq/1", keyboard_handler);
+
 	eventmanager_attach("keyboard/press/scancode",   &term_handler_press);
 	eventmanager_attach("keyboard/release/scancode", &term_handler_release);
 }
