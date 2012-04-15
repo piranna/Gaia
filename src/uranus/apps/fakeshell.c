c323080ba66db83a9da422fad6e68f49a90b4b18
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-04-15 21:27:31 +0200

    Moved eventmanager apps related code to independent file

--- src/uranus/apps/fakeshell.c
+++ src/uranus/apps/fakeshell.c
@@ -10,62 +10,11 @@
 
 #include <stdio.h>
 
-#include "eventmanager.h"
-#include "fixedQueue.h"
+#include "eventmanager_app.h"
 #include "scheudler.h"
 
 #include "echo.h"
 
-
-// Local app event manager
-static fixedDict eventmanager_events;
-static fixedQueue eventmanager_queue;
-
-static void eventmanager_dispatch_app(pairEventData* pair)
-{
-
-}
-
-static void eventmanager_send_app(char* event, int data)
-//void eventmanager_send(char* event, int data)
-{
-	pairEventData pair;
-//	strncpy(pair.event, event, sizeof(pair.event));
-	pair.data = data;
-
-//	fixedQueue_append(&eventmanager_queue, &pair);
-
-	// This should not be here, eventmanager_pumpEvents() should be used for
-	// real queued events, but since i have problems with 'VGA/text/putchar'
-	// events (race condition?) and would need some kind of locking for the
-	// queue, i dispatch it here directly by the moment
-//	eventmanager_dispatch_app(&pair);
-}
-
-static void eventmanager_attach_app(char* event, event_func func)
-//static void eventmanager_attach(char* event, event_func func)
-{
-	fixedDict_set(&eventmanager_events, event, func);
-	eventmanager_attach_delegated(event, &eventmanager_events);
-//	eventmanager_attach(event, eventmanager_send_app);
-}
-
-static void eventmanager_pumpEvents_app(void)
-//static void eventmanager_pumpEvents(void)
-{
-	// Pump events until there's no more
-	while(!fixedQueue_isEmpty(&eventmanager_queue))
-	{
-		pairEventData* pair = fixedQueue_head(&eventmanager_queue);
-
-		eventmanager_dispatch_app(pair);
-
-		fixedQueue_pop(&eventmanager_queue);
-	}
-
-	// Say we finished to pump events
-	scheudler_yield();
-}
 
 #define BUFFER_SIZE 1024
 
@@ -99,14 +48,7 @@ static void press_handler(char key)
 
 void fakeshell_init(void)
 {
-	// Init local app event manager
-	static pairKeyValue eventmanager_events_pairs[10];
-	static pairEventData eventmanager_queue_items[10];
-
-	fixedDict_init(&eventmanager_events, eventmanager_events_pairs);
-	fixedQueue_init(&eventmanager_queue, eventmanager_queue_items,
-					sizeof(pairEventData));
-	eventmanager_queue.capacity = 10;	// Hugly hack
+	eventmanager_init_app();
 
 	// Set events
 	eventmanager_attach_app("term/press/character", press_handler);
@@ -118,6 +60,7 @@ void fakeshell_init(void)
 	Process process;
 	process.send = eventmanager_send_app;
 	process.pumpEvents = eventmanager_pumpEvents_app;
+
 	scheudler_load(&process);
 }
 
---
+++ src/uranus/include/eventmanager_app.h
@@ -0,0 +1,80 @@
+/*
+ * eventmanager_app.h
+ *
+ * Functions to manage events on the apps side
+ *
+ *  Created on: 15/04/2012
+ *      Author: piranna
+ */
+
+#ifndef EVENTMANAGER_APP_H_
+#define EVENTMANAGER_APP_H_
+
+#include "eventmanager.h"
+#include "fixedDict.h"
+#include "fixedQueue.h"
+
+
+static fixedDict eventmanager_events;
+static fixedQueue eventmanager_queue;
+
+
+static void eventmanager_dispatch_app(pairEventData* pair)
+{
+
+}
+
+static void eventmanager_send_app(char* event, int data)
+//void eventmanager_send(char* event, int data)
+{
+	pairEventData pair;
+//	strncpy(pair.event, event, sizeof(pair.event));
+	pair.data = data;
+
+//	fixedQueue_append(&eventmanager_queue, &pair);
+
+	// This should not be here, eventmanager_pumpEvents() should be used for
+	// real queued events, but since i have problems with 'VGA/text/putchar'
+	// events (race condition?) and would need some kind of locking for the
+	// queue, i dispatch it here directly by the moment
+//	eventmanager_dispatch_app(&pair);
+}
+
+static void eventmanager_attach_app(char* event, event_func func)
+//static void eventmanager_attach(char* event, event_func func)
+{
+	fixedDict_set(&eventmanager_events, event, func);
+	eventmanager_attach_delegated(event, &eventmanager_events);
+//	eventmanager_attach(event, eventmanager_send_app);
+}
+
+static void eventmanager_pumpEvents_app(void)
+//static void eventmanager_pumpEvents(void)
+{
+	// Pump events until there's no more
+	while(!fixedQueue_isEmpty(&eventmanager_queue))
+	{
+		pairEventData* pair = fixedQueue_head(&eventmanager_queue);
+
+		eventmanager_dispatch_app(pair);
+
+		fixedQueue_pop(&eventmanager_queue);
+	}
+
+	// Say we finished to pump events
+	scheudler_yield();
+}
+
+static void eventmanager_init_app(void)
+{
+	// Init local app event manager
+	static pairKeyValue eventmanager_events_pairs[10];
+	static pairEventData eventmanager_queue_items[10];
+
+	fixedDict_init(&eventmanager_events, eventmanager_events_pairs);
+	fixedQueue_init(&eventmanager_queue, eventmanager_queue_items,
+					sizeof(pairEventData));
+	eventmanager_queue.capacity = 10;	// Hugly hack
+}
+
+#endif /* EVENTMANAGER_APP_H_ */
